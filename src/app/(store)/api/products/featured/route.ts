0f6b0c49c0e98ee22d70e374f79f3dfac8c4bc53
Afonso Lima <afonsolima@gmail.com>
2025-01-21 16:57:36 -0300

    home page loading with skeleton

--- src/app/(store)/api/products/featured/route.ts
+++ src/app/(store)/api/products/featured/route.ts
@@ -2,6 +2,9 @@ import data from "../data.json";
 
 export async function GET() {
   console.log("GET", "...featureds");
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   return Response.json(
     data.products.filter((product) => product.featured === true)
   );
