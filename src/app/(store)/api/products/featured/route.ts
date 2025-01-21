184fa3a4707acbca0118768f1211a31f7757d5a2
Afonso Lima <afonsolima@gmail.com>
2025-01-21 16:40:31 -0300

    caching api

--- src/app/(store)/api/products/featured/route.ts
+++ src/app/(store)/api/products/featured/route.ts
@@ -1,6 +1,7 @@
 import data from "../data.json";
 
 export async function GET() {
+  console.log("GET", "...featureds");
   return Response.json(
     data.products.filter((product) => product.featured === true)
   );
