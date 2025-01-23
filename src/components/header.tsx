c113adc144c08cfd2c69ebbc351d25bcf9ee126c
Afonso Lima <afonsolima@gmail.com>
2025-01-23 11:07:44 -0300

    adding products to cart

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
-import { Search, ShoppingBag } from "lucide-react";
+import CartWidget from "@/app/(store)/product/cart-widget";
+import { Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,10 +20,7 @@ export default function Header() {
         </form>
       </div>
       <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
-          <ShoppingBag className="h-4 w-4" />
-          <span className="text-sm">Cart (0)</span>
-        </div>
+        <CartWidget />
 
         <div className="w-px h-4 bg-zinc-700" />
 
