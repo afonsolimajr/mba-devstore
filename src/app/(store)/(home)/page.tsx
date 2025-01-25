e3f8316eb165072be9da9d46948695288ae97712
Afonso Lima <afonsolima@gmail.com>
2025-01-25 14:10:39 -0300

    responsivity on home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
   const [highLightedProduct, ...otherProducts] = await getFeaturedProducts();
 
   return (
-    <div className="grid max-h-[920px] grid-cols-9 grid-rows-6 gap-6">
+    <div className="grid max-h-[920px] grid-cols-6 grid-rows-9 grid-flow-col md:grid-cols-9 md:grid-rows-6 gap-6">
       <Link
         href={`/product/${highLightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
